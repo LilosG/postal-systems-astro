6a4e1c0a57c530fadb0c8bf892df7a5d009459bd
Michael Prickett <mprickett@bluedoorsp.com>
2025-09-23 12:10:06 -0700

    Tidy: remove router/island utilities; static home + Astro pages

--- src/lib/useClientReady.tsx
+++
@@ -1,6 +0,0 @@
-import { useEffect, useState } from 'react';
-export function useClientReady() {
-  const [ready, setReady] = useState(false);
-  useEffect(() => setReady(true), []);
-  return ready;
-}
